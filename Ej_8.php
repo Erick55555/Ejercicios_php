0ad6f7a850c6fcbfcd4b0ac65270ffd69dd95a77
Erick55555 <74232892+Erick55555@users.noreply.github.com>
2021-09-30 12:59:24 +0200

    Update Ej_8.php

--- Ej_8.php
+++ Ej_8.php
@@ -3,8 +3,12 @@
         private $num1;
 
         public function __construct($num1) {
-            $this->num1 = $num1;
-            echo "<br>Este es el numero 1: ".$num1; 
+            if($num1%2==0){
+                throw new Exception("Este numerto es par");
+            }else{
+               $this->num1 = $num1;
+               echo "<br>Este es el numero 1: ".$num1; 
+            }
         }
 
         public function construir() {
@@ -21,8 +25,14 @@
     }
 
     $num1=rand(1,15);
-
-    $bas1 = new base($num1);
-    echo $bas1->construir()
+    try{
+       $bas1 = new base($num1);
+       echo $bas1->construir();
+    }catch(Exception $e){
+        echo $e->getMessage();
+    }
+    
+    
        
+
 ?>
